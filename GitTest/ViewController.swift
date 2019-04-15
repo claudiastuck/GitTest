818cdae50e2f05206767ac758aedbd96eeb91030
claudiastuck <claudia.stuck21@gmail.com>
2019-04-15 14:21:44 -0400

    added images

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -9,13 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    // we are going to make the game odds and evens
+    // the way it works is two people one can be a person and one can be a computer
+    // put out and number of fingers you want
+    // if they add up to odd one person wins if they add up to even the other ones win
+    // kind of similar to rock paper scissors we can do easy stretches and animations like I did on my RPS and we'll get an amazing grade
+    
+    // I already made AppIcon, and inserted pictures of each finger possibility to the assets
+    // please design on iphone 8 cause then you can run it on something bigger and it works but if you design on xr and then try to run on 8 it doesn't fit at all
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        // hi
+       // you can use image literal after you make connections for the images to assign them to each image view in order from 1 to 5 the images are already labled
+    
     }
-        //hi
+    
 
 }
 
