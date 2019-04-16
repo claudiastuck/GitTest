8b30cff06fcb68951f2fecc0cf4002bcbd5828a3
emmataylor23 <emmanorcross04@yahoo.com>
2019-04-16 10:41:44 -0400

    new functions

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -8,24 +8,80 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var imageOne: UIImageView!
+    @IBOutlet weak var imageTwo: UIImageView!
+    @IBOutlet weak var imageThree: UIImageView!
+    @IBOutlet weak var ImageFour: UIImageView!
+    @IBOutlet weak var ImageFive: UIImageView!
     
+    let computerChoiceArray = [1,2,3,4,5]
+    //i'm not sure how many numbers or what numbers you put in here but i just started with these
     
-    // we are going to make the game odds and evens
-    // the way it works is two people one can be a person and one can be a computer
-    // put out and number of fingers you want
-    // if they add up to odd one person wins if they add up to even the other ones win
-    // kind of similar to rock paper scissors we can do easy stretches and animations like I did on my RPS and we'll get an amazing grade
-    
-    // I already made AppIcon, and inserted pictures of each finger possibility to the assets
-    // please design on iphone 8 cause then you can run it on something bigger and it works but if you design on xr and then try to run on 8 it doesn't fit at all
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-       // you can use image literal after you make connections for the images to assign them to each image view in order from 1 to 5 the images are already labled
-    
+    @IBAction func OneButtonTapped(_ sender: UIButton)
+    {
+        addNumbers(pickedNumber: 1)
     }
     
-
+    @IBAction func TwoButtonTapped(_ sender: UIButton)
+    {
+        addNumbers(pickedNumber: 2)
+    }
+    
+    @IBAction func ThreeButtonTapped(_ sender: UIButton)
+    {
+        addNumbers(pickedNumber: 3)
+    }
+    
+    @IBAction func FourButtonTapped(_ sender: UIButton)
+    {
+        addNumbers(pickedNumber: 4)
+    }
+    
+    @IBAction func FiveButtonTapped(_ sender: UIButton)
+    {
+        addNumbers(pickedNumber: 5)
+    }
+    //if you know an easier way to do any of this just get rid of what i did because i don't care
+    //just do it however you want
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        imageOne.image = #imageLiteral(resourceName: "one")
+        imageTwo.image = #imageLiteral(resourceName: "two")
+        imageThree.image = #imageLiteral(resourceName: "three")
+        ImageFour.image = #imageLiteral(resourceName: "four")
+        ImageFive.image = #imageLiteral(resourceName: "five")
+    }
+    
+    func addNumbers(pickedNumber : Int)
+    {
+        let computerChoice = computerChoiceArray.randomElement()
+        let sum = computerChoice! + pickedNumber
+        
+        if sum % 2 == 0
+            //even sum
+        {
+            view.backgroundColor = UIColor.red
+        }
+        
+        else
+            //odd sum
+        {
+            view.backgroundColor = UIColor.green
+        }
+        //the background color was just to see if it worked but we'll change it to be something else that says the winner
+        
+    }
+    
+    
 }
 
+
+// we are going to make the game odds and evens
+// the way it works is two people one can be a person and one can be a computer
+// put out and number of fingers you want
+// if they add up to odd one person wins if they add up to even the other ones win
+// kind of similar to rock paper scissors we can do easy stretches and animations like I did on my RPS and we'll get an amazing grade
