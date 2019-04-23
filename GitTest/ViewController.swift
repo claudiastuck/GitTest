c9a9d6527bfd1ea2e70c964d476471728070a8d5
emmataylor23 <emmanorcross04@yahoo.com>
2019-04-23 10:40:55 -0400

    new labels and colors

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-// so what i did today was add animations to each button when it's pressed to get larger and spin
-// I also added a rules link to take it to a website
-// so far we have 3 stretches and haven't even started the real project !!!!!
+
 // actually i think it's four : AppIcon, animations, rules link, and I think for using git hub
 // only one more to get a 100, and if we keep it so the colors change we will be able to have the 5 !! :)
+
+
+// i changed the background colors so you can see the text and then I added the two labels that will say what the computer chose and what the sum is
+// i also tried to figure out how to use the player's choice of even or odd but i couldn't figure out what to use so that we had access to that variable in another if statement
+// not that what I just said made any sense but i think that's the only thing really left to do
+
 
 class ViewController: UIViewController
 {
@@ -21,12 +25,11 @@ class ViewController: UIViewController
     @IBOutlet weak var imageThree: UIImageView!
     @IBOutlet weak var ImageFour: UIImageView!
     @IBOutlet weak var ImageFive: UIImageView!
+    @IBOutlet weak var sumLabel: UILabel!
+    @IBOutlet weak var computerChoiceLabel: UILabel!
+    @IBOutlet weak var evenOddTextField: UITextField!
     
     let computerChoiceArray = [1,2,3,4,5]
-    //i'm not sure how many numbers or what numbers you put in here but i just started with these
-    
-    // I put the buttons behind the images so when you tap on the image the button will then work
-    // I am going to try to add animations today based on my RPS ones for the buttons
     @IBAction func OneButtonTapped(_ sender: UIButton)
     {
         
@@ -106,9 +109,7 @@ class ViewController: UIViewController
             self.ImageFive.transform = CGAffineTransform.identity
         })
     }
-    //if you know an easier way to do any of this just get rid of what i did because i don't care
-    //just do it however you want
-    
+
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -119,22 +120,29 @@ class ViewController: UIViewController
         ImageFive.image = #imageLiteral(resourceName: "five")
     }
     
+   
     func addNumbers(pickedNumber : Int)
     {
         let computerChoice = computerChoiceArray.randomElement()
+        computerChoiceLabel.text = "The computer chose \(computerChoice!)"
         let sum = computerChoice! + pickedNumber
         
         if sum % 2 == 0
             //even sum
         {
-            view.backgroundColor = UIColor.red
+            var sum = "even"
+            view.backgroundColor = UIColor.magenta
+            sumLabel.text = "The sum of the numbers is even!"
         }
         
         else
             //odd sum
         {
+            var sum = "odd"
             view.backgroundColor = UIColor.green
+            sumLabel.text = "The sum of the numbers is odd!"
         }
+
         //the background color was just to see if it worked but we'll change it to be something else that says the winner
        
       
@@ -148,10 +156,3 @@ class ViewController: UIViewController
     }
     
 }
-
-
-// we are going to make the game odds and evens
-// the way it works is two people one can be a person and one can be a computer
-// put out and number of fingers you want
-// if they add up to odd one person wins if they add up to even the other ones win
-// kind of similar to rock paper scissors we can do easy stretches and animations like I did on my RPS and we'll get an amazing grade
